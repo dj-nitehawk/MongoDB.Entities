a19eb636e575ef0c60b1d9560cd24690afc9d440
Damith Gunner <djnitehawk83@gmail.com>
2019-04-26 12:05:38 +0530

    wip: unit tests

--- Tests/TestRelationships.cs
+++ Tests/TestRelationships.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MongoDB.Entities;
 using MongoDB.Driver.Linq;
 using System.Linq;
 
@@ -11,13 +10,34 @@ namespace MongoDB.Entities.Tests
         [TestMethod]
         public void setting_one_to_one_reference_returns_correct_entity()
         {
-            var book = new Book { Title = "sotorrce" };
-            var author = new Author { Name = "author" };
+            var book = new Book { Title = "book" };
+            var author = new Author { Name = "sotorrce" };
             author.Save();
             book.MainAuthor = author.ToReference();
             book.Save();
             var res = book.Collection()
-                          .Where()
+                          .Where(b => b.ID == book.ID)
+                          .Single()
+                          .MainAuthor.ToEntity();
+            Assert.AreEqual(author.Name, res.Name);
+        }
+
+        [TestMethod]
+        public void adding_one_to_many_references_returns_correct_entities()
+        {
+            var author = new Author { Name = "author" };
+            var book1 = new Book { Title = "aotmrrceb1" };
+            var book2 = new Book { Title = "aotmrrceb2" };
+            book1.Save(); book2.Save();
+            author.Books.Add(book1);
+            author.Books.Add(book2);
+            //todo: query and test
+        }
+
+        [TestMethod]
+        public void removing_one_to_many_references_removes_correct_entities()
+        {
+
         }
 
     }
