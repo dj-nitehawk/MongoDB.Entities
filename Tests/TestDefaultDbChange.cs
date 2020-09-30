5012508a6bf0f27bddb0379a5b93b74626316472
Damith Gunner <djnitehawk83@gmail.com>
2020-09-30 21:18:58 +0530

    ability to change default db at runtime

---
+++ Tests/TestDefaultDbChange.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace MongoDB.Entities.Tests
+{
+    [TestClass]
+    public class DefaultDatabaseChanging
+    {
+        [TestMethod]
+        public void throw_argument_null_exception()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => DB.ChangeDefaultDatabase(""));
+        }
+
+        [TestMethod]
+        public void throw_invalid_operation_exception()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => DB.ChangeDefaultDatabase("db3"));
+        }
+
+        [TestMethod]
+        public async Task returns_correct_database()
+        {
+            await DB.InitAsync("test1");
+            await DB.InitAsync("test2");
+
+            var defaultDb = DB.Database(default);
+            var database = DB.Database("test2");
+
+            DB.ChangeDefaultDatabase("test2");
+
+            var bookDb = DB.Database<Book>();
+
+            Assert.AreEqual(database.DatabaseNamespace.DatabaseName, bookDb.DatabaseNamespace.DatabaseName);
+
+            DB.ChangeDefaultDatabase(defaultDb.DatabaseNamespace.DatabaseName);
+        }
+
+        [TestMethod]
+        public async Task do_not_change_default_database_when_the_same()
+        {
+            await DB.InitAsync("test1");
+
+            var defaultDb = DB.Database(default);
+            var defaultDbName = DB.DatabaseName<Author>();
+
+            DB.ChangeDefaultDatabase(defaultDbName);
+
+            var bookDb = DB.Database<Book>();
+            Assert.AreSame(defaultDb, bookDb);
+        }
+    }
+}
