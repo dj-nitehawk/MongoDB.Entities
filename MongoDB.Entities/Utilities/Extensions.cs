1240c71b36e30089538a9d4c8afc647ba414c42d
Đĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2021-05-22 21:12:29 +0530

    db connection and existance checking

--- MongoDB.Entities/Utilities/Extensions.cs
+++ MongoDB.Entities/Utilities/Extensions.cs
@@ -401,5 +401,45 @@ namespace MongoDB.Entities
 
             property.SetValue(parent, new Many<TChild>(parent, property.Name, osProperty.Name, hasInverseAttrib));
         }
+
+        /// <summary>
+        /// Pings the mongodb server to check if it's still connectable
+        /// </summary>
+        /// <param name="timeoutSeconds">The number of seconds to keep trying</param>
+        public async static Task<bool> IsAccessibleAsync(this IMongoDatabase db, int timeoutSeconds = 5)
+        {
+            using (var cts = new CancellationTokenSource(timeoutSeconds * 1000))
+            {
+                try
+                {
+                    var res = await db.RunCommandAsync((Command<BsonDocument>)"{ping:1}", null, cts.Token).ConfigureAwait(false);
+                    return res["ok"] == 1;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if the database already exists on the mongodb server
+        /// </summary>
+        /// <param name="timeoutSeconds">The number of seconds to keep trying</param>
+        public async static Task<bool> ExistsAsync(this IMongoDatabase db, int timeoutSeconds = 5)
+        {
+            using (var cts = new CancellationTokenSource(timeoutSeconds * 1000))
+            {
+                try
+                {
+                    var dbs = await (await db.Client.ListDatabaseNamesAsync(cts.Token).ConfigureAwait(false)).ToListAsync().ConfigureAwait(false);
+                    return dbs.Contains(db.DatabaseNamespace.DatabaseName);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
