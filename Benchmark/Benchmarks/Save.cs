f5d029038b5101f9f324e24e4c9b3c4aefaefa3a
Đĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2021-06-19 09:37:22 +0530

    save partial vs update benchmark

---
+++ Benchmark/Benchmarks/Save.cs
@@ -0,0 +1,57 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Threading.Tasks;
+using MongoDB.Entities;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    public class SavePartialVsUpdate : BenchBase
+    {
+        private readonly Author author;
+
+        public SavePartialVsUpdate()
+        {
+            Initialize();
+            author = new()
+            {
+                ID = ObjectId.GenerateNewId().ToString(),
+                FirstName = "Test",
+                LastName = "Test",
+                Birthday = DateTime.UtcNow
+            };
+            author.SaveAsync().GetAwaiter().GetResult();
+        }
+
+        [Benchmark(Baseline = true)]
+        public Task Update()
+        {
+            return DB.Update<Author>()
+                .MatchID(author.ID)
+                .Modify(a => a.FirstName, "updated")
+                .Modify(a => a.LastName, "updated")
+                .ExecuteAsync();
+        }
+
+        [Benchmark]
+        public Task SavePartial()
+        {
+            return author.SaveOnlyAsync(a => new
+            {
+                a.FirstName,
+                a.LastName
+            });
+        }
+
+        public override Task MongoDB_Entities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task Official_Driver()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -7,7 +7,7 @@ namespace Benchmark
         private static void Main()
         {
             //BenchmarkRunner.Run(typeof(Program).Assembly);
-            BenchmarkRunner.Run<FindSingle>();
+            BenchmarkRunner.Run<SavePartialVsUpdate>();
         }
     }
 }
