817d8f72e29df2ccf457d2dea9f2e5b462975db7
Đĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2021-07-14 21:06:12 +0530

    extension methods for paged search

--- MongoDB.Entities/Core/Extensions.cs
+++ MongoDB.Entities/Core/Extensions.cs
@@ -176,6 +176,25 @@ namespace MongoDB.Entities
         }
 
         /// <summary>
+        /// Starts a paged search pipeline for this fluent pipeline
+        /// </summary>
+        /// <typeparam name="T">Any class that implements IEntity</typeparam>
+        /// <typeparam name="TProjection">The type of the resulting projection</typeparam>
+        public static PagedSearch<T, TProjection> PagedSearch<T, TProjection>(this IAggregateFluent<T> aggregate) where T : IEntity
+        {
+            return DB.PagedSearch<T, TProjection>().WithFluent(aggregate);
+        }
+
+        /// <summary>
+        /// Starts a paged search pipeline for this fluent pipeline
+        /// </summary>
+        /// <typeparam name="T">Any class that implements IEntity</typeparam>
+        public static PagedSearch<T, T> PagedSearch<T>(this IAggregateFluent<T> aggregate) where T : IEntity
+        {
+            return DB.PagedSearch<T, T>().WithFluent(aggregate);
+        }
+
+        /// <summary>
         /// Returns a reference to this entity.
         /// </summary>
         public static One<T> ToReference<T>(this T entity) where T : IEntity
