d079be49fefed57178ea1ed836b313f41220591c
Damith G <djnitehawk83@gmail.com>
2021-10-30 23:13:30 +0530

    wip

--- MongoDB.Entities/DBContext/DBContext.cs
+++ MongoDB.Entities/DBContext/DBContext.cs
@@ -24,12 +24,13 @@ namespace MongoDB.Entities
 
         private static Type[] allEntitiyTypes;
         private Dictionary<Type, (object filterDef, bool prepend)> globalFilters;
-        private readonly string tenantPrefix;
+        private string tenantPrefix;
 
         /// <summary>
         /// Initializes a DBContext instance with the given connection parameters.
         /// <para>TIP: network connection is deferred until the first actual operation.</para>
         /// </summary>
+        /// <param name="multiTenancyMode">Set this to true when calling this constructor</param>
         /// <param name="tenantPrefix">A tenant prefix value. Database names for each tenant will have this value prefixed to them</param>
         /// <param name="dbName">Name of the database</param>
         /// <param name="host">Address of the MongoDB server</param>
@@ -38,8 +39,11 @@ namespace MongoDB.Entities
         /// When supplied, all save/update operations performed via this DBContext instance will set the value on entities that has a property of type ModifiedBy. 
         /// You can even inherit from the ModifiedBy class and add your own properties to it. 
         /// Only one ModifiedBy property is allowed on a single entity type.</param>
-        public DBContext(string tenantPrefix, string dbName, string host = "127.0.0.1", int port = 27017, ModifiedBy modifiedBy = null)
+        public DBContext(bool multiTenancyMode, string tenantPrefix, string dbName, string host = "127.0.0.1", int port = 27017, ModifiedBy modifiedBy = null)
         {
+            if (!multiTenancyMode)
+                throw new ArgumentException("multiTenancyMode should be set to True");
+
             DB.Initialize(
                new MongoClientSettings { Server = new MongoServerAddress(host, port) },
                $"{tenantPrefix}_{dbName}",
@@ -78,6 +82,7 @@ namespace MongoDB.Entities
         /// Initializes a DBContext instance with the given connection parameters.
         /// <para>TIP: network connection is deferred until the first actual operation.</para>
         /// </summary>
+        /// <param name="multiTenancyMode">Set this to true when calling this constructor</param>
         /// <param name="tenantPrefix">A tenant prefix value. Database names for each tenant will have this value prefixed to them</param>
         /// <param name="dbName">Name of the database</param>
         /// <param name="settings">A MongoClientSettings object</param>
@@ -85,8 +90,11 @@ namespace MongoDB.Entities
         /// When supplied, all save/update operations performed via this DBContext instance will set the value on entities that has a property of type ModifiedBy. 
         /// You can even inherit from the ModifiedBy class and add your own properties to it. 
         /// Only one ModifiedBy property is allowed on a single entity type.</param>
-        public DBContext(string tenantPrefix, string dbName, MongoClientSettings settings, ModifiedBy modifiedBy = null)
+        public DBContext(bool multiTenancyMode, string tenantPrefix, string dbName, MongoClientSettings settings, ModifiedBy modifiedBy = null)
         {
+            if (!multiTenancyMode)
+                throw new ArgumentException("multiTenancyMode should be set to True");
+
             DB.Initialize(settings, $"{tenantPrefix}_{dbName}", true)
               .GetAwaiter()
               .GetResult();
