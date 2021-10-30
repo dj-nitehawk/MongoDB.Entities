d023e8dad3434c982e9655781cdb476c38a90cf9
Damith G <djnitehawk83@gmail.com>
2021-10-30 20:51:19 +0530

    wip

---
+++ MongoDB.Entities/DBContext/DBContext.File.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+
+namespace MongoDB.Entities
+{
+    public partial class DBContext
+    {
+        /// <summary>
+        /// Returns a DataStreamer object to enable uploading/downloading file data directly by supplying the ID of the file entity
+        /// </summary>
+        /// <typeparam name="T">The file entity type</typeparam>
+        /// <param name="ID">The ID of the file entity</param>
+        public DataStreamer File<T>(string ID) where T : FileEntity, new()
+        {
+            return DB.File<T>(ID, tenantPrefix);
+        }
+    }
+}
