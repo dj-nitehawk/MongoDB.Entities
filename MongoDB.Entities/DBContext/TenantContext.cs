c6865058472f8a646ba46dc0cc8ffc33c79aaec9
Damith G <djnitehawk83@gmail.com>
2021-10-31 23:32:16 +0530

    add tenant context

--- MongoDB.Entities/DBContext/TenantContext.cs
+++ MongoDB.Entities/DBContext/TenantContext.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Driver;
+using System.Threading.Tasks;
 
 namespace MongoDB.Entities
 {
@@ -8,49 +9,48 @@ namespace MongoDB.Entities
     public class TenantContext : DBContext
     {
         /// <summary>
-        /// Initializes a DBContext instance with the given connection parameters.
+        /// Instantiate a tenant context with the given tenant prefix value.
+        /// </summary>
+        /// <param name="tenantPrefix">The tenant prefix to be prepended to database names.</param>
+        public TenantContext(string tenantPrefix)
+        {
+            this.tenantPrefix = tenantPrefix;
+        }
+
+        /// <summary>
+        /// Configure this tenant context to be able to connect to a particular database/server.
         /// <para>TIP: network connection is deferred until the first actual operation.</para>
         /// </summary>
-        /// <param name="tenantPrefix">A tenant prefix value. Database names for each tenant will have this value prefixed to them</param>
-        /// <param name="dbName">Name of the database</param>
+        /// <param name="dbName">Name of the database (without tenant prefix)</param>
         /// <param name="host">Address of the MongoDB server</param>
         /// <param name="port">Port number of the server</param>
         /// <param name="modifiedBy">An optional ModifiedBy instance. 
         /// When supplied, all save/update operations performed via this DBContext instance will set the value on entities that has a property of type ModifiedBy. 
         /// You can even inherit from the ModifiedBy class and add your own properties to it. 
         /// Only one ModifiedBy property is allowed on a single entity type.</param>
-        public TenantContext(string tenantPrefix, string dbName, string host = "127.0.0.1", int port = 27017, ModifiedBy modifiedBy = null)
+        public Task InitAsync(string dbName, string host = "127.0.0.1", int port = 27017, ModifiedBy modifiedBy = null)
         {
-            DB.Initialize(
-               new MongoClientSettings { Server = new MongoServerAddress(host, port) },
-               $"{tenantPrefix}~{dbName}",
-               true)
-             .GetAwaiter()
-             .GetResult();
-
             ModifiedBy = modifiedBy;
-            this.tenantPrefix = tenantPrefix;
+            return DB.Initialize(
+                new MongoClientSettings { Server = new MongoServerAddress(host, port) },
+                $"{tenantPrefix}~{dbName}",
+                true);
         }
 
         /// <summary>
-        /// Initializes a DBContext instance with the given connection parameters.
+        /// Configure this tenant context to be able to connect to a particular database/server.
         /// <para>TIP: network connection is deferred until the first actual operation.</para>
         /// </summary>
-        /// <param name="tenantPrefix">A tenant prefix value. Database names for each tenant will have this value prefixed to them</param>
-        /// <param name="dbName">Name of the database</param>
+        /// <param name="dbName">Name of the database (without tenant prefix)</param>
         /// <param name="settings">A MongoClientSettings object</param>
         /// <param name="modifiedBy">An optional ModifiedBy instance. 
         /// When supplied, all save/update operations performed via this DBContext instance will set the value on entities that has a property of type ModifiedBy. 
         /// You can even inherit from the ModifiedBy class and add your own properties to it. 
         /// Only one ModifiedBy property is allowed on a single entity type.</param>
-        public TenantContext(string tenantPrefix, string dbName, MongoClientSettings settings, ModifiedBy modifiedBy = null)
+        public Task InitAsync(string dbName, MongoClientSettings settings, ModifiedBy modifiedBy = null)
         {
-            DB.Initialize(settings, $"{tenantPrefix}~{dbName}", true)
-              .GetAwaiter()
-              .GetResult();
-
             ModifiedBy = modifiedBy;
-            this.tenantPrefix = tenantPrefix;
+            return DB.Initialize(settings, $"{tenantPrefix}~{dbName}", true);
         }
     }
 }
