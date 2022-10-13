4fc7eb528856deafe67d8be9971cf9d17a18da5d
Erik <reachej@gmail.com>
2022-10-13 15:57:00 -0500

    Update DBContext.GeoNear.cs
    distanceField and includeLocs need to be strings.   the Limit option doesn't work (something changed on Mongo's side)

--- MongoDB.Entities/DBContext/DBContext.GeoNear.cs
+++ MongoDB.Entities/DBContext/DBContext.GeoNear.cs
@@ -23,7 +23,7 @@ public partial class DBContext
     /// <param name="options">The options for the aggregation. This is not required.</param>
     /// <typeparam name="T">The type of entity</typeparam>
     /// <param name="ignoreGlobalFilters">Set to true if you'd like to ignore any global filters for this operation</param>
-    public IAggregateFluent<T> GeoNear<T>(Coordinates2D NearCoordinates, Expression<Func<T, object>> DistanceField, bool Spherical = true, int? MaxDistance = null, int? MinDistance = null, int? Limit = null, BsonDocument Query = null, int? DistanceMultiplier = null, Expression<Func<T, object>> IncludeLocations = null, string IndexKey = null, AggregateOptions options = null, bool ignoreGlobalFilters = false) where T : IEntity
+           public IAggregateFluent<T> GeoNear<T>(Coordinates2D NearCoordinates, string DistanceField = null, bool Spherical = true, int? MaxDistance = null, int? MinDistance = null, int? Limit = null, BsonDocument Query = null, int? DistanceMultiplier = null, string IncludeLocations = null, string IndexKey = null, AggregateOptions options = null, bool ignoreGlobalFilters = false) where T : IEntity
     {
         var globalFilter = Logic.MergeWithGlobalFilter(ignoreGlobalFilters, globalFilters, Builders<T>.Filter.Empty);
 
