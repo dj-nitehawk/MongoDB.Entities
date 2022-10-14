c805381e5571f7ab204ef30daf6f84807a5a44c7
Erik <reachej@gmail.com>
2022-10-13 21:05:22 -0500

    Update DB.GeoNear.cs

--- MongoDB.Entities/DB/DB.GeoNear.cs
+++ MongoDB.Entities/DB/DB.GeoNear.cs
@@ -3,40 +3,41 @@ using MongoDB.Driver;
 using System;
 using System.Linq.Expressions;
 
-namespace MongoDB.Entities;
-
-public static partial class DB
+namespace MongoDB.Entities
 {
-    /// <summary>
-    /// Start a fluent aggregation pipeline with a $GeoNear stage with the supplied parameters.
-    /// </summary>
-    /// <param name="NearCoordinates">The coordinates from which to find documents from</param>
-    /// <param name="DistanceField">x => x.Distance</param>
-    /// <param name="Spherical">Calculate distances using spherical geometry or not</param>
-    /// <param name="MaxDistance">The maximum distance in meters from the center point that the documents can be</param>
-    /// <param name="MinDistance">The minimum distance in meters from the center point that the documents can be</param>
-    /// <param name="Limit">The maximum number of documents to return</param>
-    /// <param name="Query">Limits the results to the documents that match the query</param>
-    /// <param name="DistanceMultiplier">The factor to multiply all distances returned by the query</param>
-    /// <param name="IncludeLocations">Specify the output field to store the point used to calculate the distance</param>
-    /// <param name="IndexKey"></param>
-    /// <param name="options">The options for the aggregation. This is not required.</param>
-    /// <param name="session">An optional session if using within a transaction</param>
-    public static IAggregateFluent<T> FluentGeoNear<T>(Coordinates2D NearCoordinates, Expression<Func<T, object>> DistanceField, bool Spherical = true, double? MaxDistance = null, double? MinDistance = null, int? Limit = null, BsonDocument Query = null, double? DistanceMultiplier = null, Expression<Func<T, object>> IncludeLocations = null, string IndexKey = null, AggregateOptions options = null, IClientSessionHandle session = null) where T : IEntity
+    public static partial class DB
     {
-        return new GeoNear<T>
+        /// <summary>
+        /// Start a fluent aggregation pipeline with a $GeoNear stage with the supplied parameters.
+        /// </summary>
+        /// <param name="NearCoordinates">The coordinates from which to find documents from</param>
+        /// <param name="DistanceField"> string of the field name in dot notation, lowercase property
+        /// <param name="Spherical">Calculate distances using spherical geometry or not</param>
+        /// <param name="MaxDistance">The maximum distance in meters from the center point that the documents can be</param>
+        /// <param name="MinDistance">The minimum distance in meters from the center point that the documents can be</param>
+        /// <param name="Limit">The maximum number of documents to return</param>
+        /// <param name="Query">Limits the results to the documents that match the query</param>
+        /// <param name="DistanceMultiplier">The factor to multiply all distances returned by the query</param>
+        /// <param name="IncludeLocations">Specify the output field to store the point used to calculate the distance</param>
+        /// <param name="IndexKey"> string of the field name in dot notation, lowercase property
+        /// <param name="options">The options for the aggregation. This is not required.</param>
+        /// <param name="session">An optional session if using within a transaction</param>
+        public static IAggregateFluent<T> FluentGeoNear<T>(Coordinates2D NearCoordinates, string DistanceField = null, bool Spherical = true, double? MaxDistance = null, double? MinDistance = null, int? Limit = null, BsonDocument Query = null, double? DistanceMultiplier = null, string IncludeLocations = null, string IndexKey = null, AggregateOptions options = null, IClientSessionHandle session = null) where T : IEntity
         {
-            near = NearCoordinates,
-            distanceField = DistanceField?.FullPath(),
-            spherical = Spherical,
-            maxDistance = MaxDistance,
-            minDistance = MinDistance,
-            query = Query,
-            distanceMultiplier = DistanceMultiplier,
-            limit = Limit,
-            includeLocs = IncludeLocations?.FullPath(),
-            key = IndexKey,
+            return new GeoNear<T>
+            {
+                near = NearCoordinates,
+                distanceField = DistanceField,
+                spherical = Spherical,
+                maxDistance = MaxDistance,
+                minDistance = MinDistance,
+                query = Query,
+                distanceMultiplier = DistanceMultiplier,
+                limit = Limit,
+                includeLocs = IncludeLocations,
+                key = IndexKey,
+            }
+            .ToFluent(options, session);
         }
-        .ToFluent(options, session);
     }
 }
