f57e9e0785d50a42635be2f7fb24f8b973bdf9e5
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-09-20 16:51:12 +0530

    organize core folder

---
+++ MongoDB.Entities/Core/Entities/SequenceCounter.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB.Entities;
+
+[Collection("[SEQUENCE_COUNTERS]")]
+internal class SequenceCounter : IEntity
+{
+    [BsonId] public string? ID { get; set; }
+
+    [BsonRepresentation(BsonType.Int64)]
+    public ulong Count { get; set; }
+
+    public object GenerateNewID()
+        => throw new System.NotImplementedException();
+}
