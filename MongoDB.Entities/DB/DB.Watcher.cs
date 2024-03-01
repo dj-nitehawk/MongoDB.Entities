ba834064d5d55483abc8cb99e05769a2cc46f69e
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2024-03-01 18:13:25 +0530

    upgrade dependencies & cleanups

--- MongoDB.Entities/DB/DB.Watcher.cs
+++ MongoDB.Entities/DB/DB.Watcher.cs
@@ -5,24 +5,19 @@ namespace MongoDB.Entities;
 public static partial class DB
 {
     /// <summary>
-    /// Retrieves the 'change-stream' watcher instance for a given unique name. 
-    /// If an instance for the name does not exist, it will return a new instance. 
+    /// Retrieves the 'change-stream' watcher instance for a given unique name.
+    /// If an instance for the name does not exist, it will return a new instance.
     /// If an instance already exists, that instance will be returned.
     /// </summary>
     /// <typeparam name="T">The entity type to get a watcher for</typeparam>
     /// <param name="name">A unique name for the watcher of this entity type. Names can be duplicate among different entity types.</param>
     public static Watcher<T> Watcher<T>(string name) where T : IEntity
-    {
-        // Normalize the name once
-        var normalizedName = name.ToLower().Trim();
-
-        // Use GetOrAdd to ensure thread-safe retrieval or addition of the watcher.
-        return Cache<T>.Watchers.GetOrAdd(normalizedName, newName => new Watcher<T>(newName));
-    }
+        => Cache<T>.Watchers.GetOrAdd(name.ToLower().Trim(), newName => new(newName));
 
     /// <summary>
     /// Returns all the watchers for a given entity type
     /// </summary>
     /// <typeparam name="T">The entity type to get the watcher of</typeparam>
-    public static IEnumerable<Watcher<T>> Watchers<T>() where T : IEntity => Cache<T>.Watchers.Values;
+    public static IEnumerable<Watcher<T>> Watchers<T>() where T : IEntity
+        => Cache<T>.Watchers.Values;
 }
--- changelog.md
+++ changelog.md
@@ -2,6 +2,7 @@
 
 ### IMPROVEMENTS
 
-- upgrade mongodb driver to v2.23
+- upgrade mongodb driver to v2.24
+- minor internal refactors
 
 [//]: # (### BREAKING CHANGES)
