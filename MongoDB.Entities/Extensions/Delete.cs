45d3a5d016cec3c77e6a60bb16bac9d9602decfc
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2025-12-29 20:43:16 +0530

    update changelog

--- MongoDB.Entities/Extensions/Delete.cs
+++ MongoDB.Entities/Extensions/Delete.cs
@@ -8,15 +8,15 @@ namespace MongoDB.Entities;
 
 public static partial class Extensions
 {
-    // /// <summary>
-    // /// Deletes a single entity from MongoDB.
-    // /// <para>HINT: If this entity is referenced by one-to-many/many-to-many relationships, those references are also deleted.</para>
-    // /// </summary>
-    // /// <param name="entity"></param>
-    // /// <param name="db">The DB instance to use for this operation</param>
-    // /// <param name="cancellation">An optional cancellation token</param>
-    // public static Task<DeleteResult> DeleteAsync<T>(this T entity, DB db, CancellationToken cancellation = default) where T : IEntity
-    //     => db.DeleteAsync<T>(entity.GetId(), cancellation);
+    /// <summary>
+    /// Deletes a single entity from MongoDB.
+    /// <para>HINT: If this entity is referenced by one-to-many/many-to-many relationships, those references are also deleted.</para>
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <param name="db">The DB instance to use for this operation</param>
+    /// <param name="cancellation">An optional cancellation token</param>
+    public static Task<DeleteResult> DeleteAsync<T>(this T entity, DB db, CancellationToken cancellation = default) where T : IEntity
+        => db.DeleteAsync<T>(entity.GetId(), cancellation);
 
     /// <summary>
     /// Deletes multiple entities from the database
--- changelog.md
+++ changelog.md
@@ -1,7 +1,9 @@
 [//]: # (### NEW)
 
-### IMPROVEMENTS
+[//]: # (### IMPROVEMENTS)
 
-- upgrade mongodb driver to latest.
+# ⚠️ Major Version With Breaking Changes ⚠️
 
-[//]: # (### BREAKING CHANGES)
+Version 25 is a major version jump with many breaking changes. It is not recommended to upgrade to v25 unless you have the time to go through the [latest documentation](https://mongodb-entities.com/) and spend some effort to upgrade your existing project code. It's not that complicated to upgrade but will involve a lot of manual work if your codebase is quite large. AI could possibly help with that.
+
+v25 modernizes the API and brings proper multi-database support. The old way of doing things via `DB` static calls is gone. All operations must now be performed via a `DB` instance. The extension methods targeting entities such as `something.SaveAsync()` are no longer available. The features that were tied to `DBContext` has also been merged in to the `DB` class.
